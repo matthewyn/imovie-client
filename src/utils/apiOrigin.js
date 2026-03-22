408a910234bcd2ac7bdeedd2903615ab3ed436d3
Kenneth Matthew Yonathan <kennethmatthewyonathan@Kenneths-MacBook-Air.local>
2026-03-22 16:24:12 +0700

    Fix origin URL request

--- src/utils/apiOrigin.js
+++ src/utils/apiOrigin.js
@@ -2,6 +2,6 @@ export function generateApiOrigin(path) {
   if (process.env.NODE_ENV === "development") {
     return `http://localhost:3000${path}`;
   } else {
-    return `${process.env.BACKEND_API}${path}`;
+    return `${import.meta.env.VITE_BACKEND_API}${path}`;
   }
 }
